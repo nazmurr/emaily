3093b1e38ea8af47ea8153daf5e5cc0614762511
NazmurR <nazmur.r@gmail.com>
2020-06-23 02:00:45 +0600

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+//start db cmd: /Users/nazmur21/mongodb/bin/mongod --dbpath=/Users/nazmur21/mongodb-data
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
